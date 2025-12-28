4ec5ea779c25a61e32d98ade14a914b9add82fdb
Phạm Bảo Kha <162149048+MichaelPham2005@users.noreply.github.com>
2025-12-28 23:37:23 +0700

    Modify parameters

--- tkbc/scripts/train_continuous_pairre.sh
+++ tkbc/scripts/train_continuous_pairre.sh
@@ -7,10 +7,10 @@
 python learner.py \
     --dataset ICEWS14 \
     --model ContinuousPairRE \
-    --rank 100 \
+    --rank 128 \
     --batch_size 1000 \
     --learning_rate 0.1 \
-    --max_epochs 50 \
-    --valid_freq 5 \
-    --emb_reg 0.001 \
-    --time_reg 0.001
+    --max_epochs 250 \
+    --valid_freq 25 \
+    --emb_reg 0.01 \
+    --time_reg 0.01
